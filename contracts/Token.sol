68bd46b7164640250be3944bda74d3ff73d84cd6
Nicolas Murphy <murphynick10@gmail.com>
2024-05-02 20:02:46 -0500

    adjust token minting

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     constructor() ERC20("SampleToken", "STK") {
-        _mint(msg.sender, 1000);
+        _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
     }
 }
