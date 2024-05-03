874ae973daded560e0eb777cb2b5e8b33bfe4cef
Nicolas Murphy <murphynick10@gmail.com>
2024-05-02 19:26:05 -0500

    downgraded node, openzeppelin, deployed contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     constructor() ERC20("SampleToken", "STK") {
-        _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
+        _mint(msg.sender, 1000);
     }
 }
